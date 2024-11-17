c9e7a57d325c885d4c3234ea0c89fda60fdd7ec1
chetbae <48145854+chetbae@users.noreply.github.com>
2024-11-17 00:02:23 -0500

    Remove log of teleportation

--- app/scripts/characters/ghost.js
+++ app/scripts/characters/ghost.js
@@ -27,9 +27,6 @@ class Ghost {
       top: (gridY - 0.5) * this.scaledTileSize,
     };
 
-    // Verbose logging (for demo?)
-    console.log(`Teleporting ${this.name} to ${gridX}, ${gridY}`);
-
     // Update the physics positions
     this.position = Object.assign({}, newPosition);
     this.oldPosition = Object.assign({}, newPosition);
